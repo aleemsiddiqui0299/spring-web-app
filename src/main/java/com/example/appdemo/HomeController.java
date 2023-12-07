10db176bb2ff22e708976c0cb9e82e6d9de97878
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2023-12-07 09:58:59 +0530

    list service add

--- src/main/java/com/example/appdemo/HomeController.java
+++ src/main/java/com/example/appdemo/HomeController.java
@@ -1,14 +1,29 @@
 package com.example.appdemo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HomeController {
+
+
+    @Autowired
+            @Qualifier("students")
+    UserService userService;
 
     @GetMapping("/")
     public String hello(){
         return "hello world";
     }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return userService.getStudents();
+    }
+
 }
